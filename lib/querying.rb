2785f1c0e3241dd7b0f403185d46fa56da334bb0
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-07-03 22:16:58 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,7 +4,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   WHERE series_id = 1
   ORDER BY year;"
 end
- 
+
 def select_name_and_motto_of_char_with_longest_motto
   "Write your SQL query here"
   "SELECT name, motto
@@ -12,14 +12,14 @@ def select_name_and_motto_of_char_with_longest_motto
   ORDER BY motto
   LIMIT 1;"
 end
- 
- 
+
+
 def select_value_and_count_of_most_prolific_species
   "Write your SQL query here"
   "SELECT species, COUNT(*) FROM characters
   GROUP BY species ORDER BY COUNT(*) DESC LIMIT 1;"
 end
- 
+
 def select_name_and_series_subgenres_of_authors
   "Write your SQL query here"
   "SELECT Authors.name, Subgenres.name
@@ -29,7 +29,7 @@ def select_name_and_series_subgenres_of_authors
   JOIN subgenres
   ON subgenres.id = series.subgenre_id;"
 end
- 
+
 def select_series_title_with_most_human_characters
   "Write your SQL query here"
   "SELECT Series.title
@@ -38,7 +38,7 @@ def select_series_title_with_most_human_characters
   ON Characters.series_id = Series.id
   GROUP BY Series.title LIMIT 1;"
 end
- 
+
 def select_character_names_and_number_of_books_they_are_in
 "Write your SQL query here"
 "SELECT Characters.name, COUNT(*) as book_count
